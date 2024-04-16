3b0ebc85849b668431dcdac810552b71ecda04ee
artemii <kavrese@gmail.com>
2024-04-16 17:42:27 +0300

    added dynamic water line to humidity_page.dart

--- lib/humidity_page.dart
+++ lib/humidity_page.dart
@@ -11,12 +11,13 @@ class HumiditySensorPage extends StatefulWidget {
 }
 
 class _HumiditySensorPageState extends State<HumiditySensorPage> {
-
   bool isError = false;
   bool isLoading = true;
   double humidity = 0;
   StreamSubscription<double>? _stream;
   final environmentSensors = EnvironmentSensors();
+
+  late double screenHeight;
 
   @override
   void initState() {
@@ -27,7 +28,7 @@ class _HumiditySensorPageState extends State<HumiditySensorPage> {
         isError = !value;
       });
 
-      if (value){
+      if (value) {
         _stream = environmentSensors.humidity.listen((event) {
           setState(() {
             humidity = event;
@@ -37,7 +38,6 @@ class _HumiditySensorPageState extends State<HumiditySensorPage> {
     });
   }
 
-
   @override
   void dispose() {
     super.dispose();
@@ -46,14 +46,53 @@ class _HumiditySensorPageState extends State<HumiditySensorPage> {
 
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Center(
+      body: Stack(children: [
+        SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: Colors.blueAccent,
+              width: double.infinity,
+              height: screenHeight * (humidity / 100),
+            ),
+          ),
+        ),
+        Center(
           child: (isLoading)
-              ? const CircularProgressIndicator()
-              : (isError)
+            ? const CircularProgressIndicator()
+            : (isError)
                 ? const Text("Датчик влажности не обнаружен")
-                : Text(humidity.toString()),
-        )
-    );
+                : Text(
+                    humidity.toString(),
+                    style: const TextStyle(
+                        fontSize: 42,
+                        color: Colors.blueAccent,
+                        inherit: true,
+                        shadows: [
+                          Shadow(
+                              offset: Offset(-1.5, -1.5),
+                              color: Colors.white
+                          ),
+                          Shadow(
+                              offset: Offset(1.5, -1.5),
+                              color: Colors.white
+                          ),
+                          Shadow(
+                              offset: Offset(1.5, 1.5),
+                              color: Colors.white
+                          ),
+                          Shadow(
+                              offset: Offset(-1.5, 1.5),
+                              color: Colors.white
+                          ),
+                        ]
+                    )
+                  ),
+        ),
+    ]));
   }
 }
